078b7842d3b0bdae2071332284466ef1cba9ca65
TH <161852406+0xRech@users.noreply.github.com>
2026-09-04 21:11:33 +0200

    website: make RGX branding fully static

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 (() => {
-  /* Replace only the favicon. Branding/layout are handled purely by CSS/HTML. */
-  document.querySelectorAll('link[rel~="icon"],link[rel="shortcut icon"]').forEach(link => link.remove());
-  const icon = document.createElement('link');
-  icon.rel = 'icon';
-  icon.type = 'image/png';
-  icon.sizes = '128x128';
-  icon.href = '/assets/favicon-rgx-v5.png?v=20260904-6';
-  document.head.appendChild(icon);
-
   const base = document.createElement('script');
   base.src = '/script-base-v6.js?v=20260904-6';
   base.defer = true;
